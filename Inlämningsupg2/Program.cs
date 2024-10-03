a83833fd4df3f54f74c46d940c1d233febf55b51
nehy06001 <nehy06001@E-PW05CGQV>
2024-10-03 14:23:30 +0200

    Add project files.

---
+++ Inlämningsupg2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Inlämningsupg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i meter?");
+            int Elin = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hoppade Alma i meter?");
+            int Alma = int.Parse(Console.ReadLine());
+
+            int svar = Elin - Alma;
+            Console.WriteLine("Elin hoppade " + svar + " meter högre än Alma");
+        }
+    }
+}
